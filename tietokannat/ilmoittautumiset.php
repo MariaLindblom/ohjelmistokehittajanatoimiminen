555474073d46040281564127a5067a6fd85a4764
MariaLindblom <ml6581@edu.turku.fi>
2022-09-29 15:28:20 +0300

    lisää php:tä

--- tietokannat/ilmoittautumiset.php
+++ tietokannat/ilmoittautumiset.php
@@ -1,19 +1,26 @@
 <?php
-try {
-    $pdo = new PDO('mysgl:host=localhost;dbname:20n_1900', '20n_1900', 'Xm[9ZohbE@OPQovp');
-} catch (PDOException $e) {
-    exit('tietokantavirhe');
+// Alla olevat testejä varten, lopullisesta pois tietoturvan takia
+@ini_set("display_errors", 1);
+@ini_set("error_reporting", E_ALL);
+// Otetaan yhteys tietokantapalvelimeen
+include("yhteys.php");
+// versio 1: haetaan tekstilaatikoiden sisällöt muuttujiin
+$etunimi = $_POST["enimi"];
+$sukunimi = $_POST["snimi"];
+$sahkoposti = $_POST["sposti"];
+$koulutalo = $_POST["koulu"];
+$valinta = $_POST["kurssit"];
+$viesti = $_POST["viesti"];
+echo "$etunimi .. $sukunimi .. $sahkoposti .. $koulutalo .. $valinta .. $viesti <br>";
+// Versio 2: muodostetaan SQL-lause ja echotetaan se, jotta voidaan tarkistaa onko ok
+$sql = "INSERT INTO taulukko (Etunimi, Sukunimi, Sahkoposti, Koulutalo, Kurssivalinta, Viesti) 
+VALUES ('$etunimi', '$sukunimi', '$sahkoposti', '$koulutalo', '$valinta', '$viesti')";
+// echo $sql;
+try { 
+		$tiedot = $yhteys->prepare($sql); 
+		$tiedot->execute(); 
+	} catch (PDOException $e) { 
+	die("VIRHE NRO 2: " . $e->getMessage()); 
 }
-
-/* $server = '194.197.245.5';
-$user = '20n_1900';
-$pass = 'Xm[9ZohbE@OPQovp';
-
-$connect = ftp_connect($server) or die('Yhdistyminen ei onnistunut.');
-
-if(ftp_login($connect, $user, $pass)) {
-    echo 'Yhdistyminen onnistui.';
-}
-*/
 
 ?>
---
+++ tietokannat/yhteys.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname:20n_1900', '20n_1900', 'Xm[9ZohbE@OPQovp');
+} catch (PDOException $e) {
+    exit('tietokantavirhe');
+}
+?>
