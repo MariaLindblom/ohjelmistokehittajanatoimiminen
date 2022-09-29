86a3c28a8893f4fc607c7739fd4f9e1075cfd1cb
MariaLindblom <ml6581@edu.turku.fi>
2022-09-29 12:19:53 +0300

    Update ilmoittautumiset.php

--- tietokannat/ilmoittautumiset.php
+++ tietokannat/ilmoittautumiset.php
@@ -1,5 +1,11 @@
 <?php
-$server = '194.197.245.5';
+try {
+    $pdo = new PDO('mysgl:host=localhost;dbname:20n_1900', '20n_1900', 'Xm[9ZohbE@OPQovp');
+} catch (PDOException $e) {
+    exit('tietokantavirhe');
+}
+
+/* $server = '194.197.245.5';
 $user = '20n_1900';
 $pass = 'Xm[9ZohbE@OPQovp';
 
@@ -8,5 +14,6 @@ $connect = ftp_connect($server) or die('Yhdistyminen ei onnistunut.');
 if(ftp_login($connect, $user, $pass)) {
     echo 'Yhdistyminen onnistui.';
 }
+*/
 
 ?>
