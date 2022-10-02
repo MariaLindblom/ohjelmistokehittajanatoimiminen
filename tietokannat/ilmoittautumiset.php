eb69569e28dc7340df18b5a9661ae7b28ca0cb3b
MariaLindblom <ml6581@edu.turku.fi>
2022-10-02 16:38:29 +0300

    lomake toimii

--- tietokannat/ilmoittautumiset.php
+++ tietokannat/ilmoittautumiset.php
@@ -10,7 +10,7 @@ $conn = new mysqli('localhost','20n_1900','Xm[9ZohbE@OPQovp','20n_1900');
 if($conn->connect_error){
 	die('Yhdistyminen ei onnistunut : '.$conn->connect_error);
 }else{
-	$stmt = $conn->prepare("INSERT INTO 20n_1900.ilmoittumiset(etunimi, sukunimi, sahkoposti, koulutalo, valinta, viesti)
+	$stmt = $conn->prepare("INSERT INTO ilmoittumiset(etunimi, sukunimi, sahkoposti, koulutalo, valinta, viesti)
 	VALUES(?, ?, ?, ?, ?, ?)");
 	$stmt->bind_param("ssssss",$etunimi, $sukunimi, $sahkoposti, $koulutalo, $valinta, $viesti);
 	$stmt->execute();
