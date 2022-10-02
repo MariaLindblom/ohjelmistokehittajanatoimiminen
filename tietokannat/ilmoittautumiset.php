8d2547371d6111412d652b7cde550706e01a05ee
MariaLindblom <ml6581@edu.turku.fi>
2022-10-02 16:23:38 +0300

    ilmoittumislomaketta

--- tietokannat/ilmoittautumiset.php
+++ tietokannat/ilmoittautumiset.php
@@ -1,26 +1,22 @@
 <?php
-// Alla olevat testejä varten, lopullisesta pois tietoturvan takia
-@ini_set("display_errors", 1);
-@ini_set("error_reporting", E_ALL);
-// Otetaan yhteys tietokantapalvelimeen
-include("yhteys.php");
-// versio 1: haetaan tekstilaatikoiden sisällöt muuttujiin
 $etunimi = $_POST["enimi"];
 $sukunimi = $_POST["snimi"];
 $sahkoposti = $_POST["sposti"];
 $koulutalo = $_POST["koulu"];
 $valinta = $_POST["kurssit"];
 $viesti = $_POST["viesti"];
-echo "$etunimi .. $sukunimi .. $sahkoposti .. $koulutalo .. $valinta .. $viesti <br>";
-// Versio 2: muodostetaan SQL-lause ja echotetaan se, jotta voidaan tarkistaa onko ok
-$sql = "INSERT INTO taulukko (Etunimi, Sukunimi, Sahkoposti, Koulutalo, Kurssivalinta, Viesti) 
-VALUES ('$etunimi', '$sukunimi', '$sahkoposti', '$koulutalo', '$valinta', '$viesti')";
-// echo $sql;
-try { 
-		$tiedot = $yhteys->prepare($sql); 
-		$tiedot->execute(); 
-	} catch (PDOException $e) { 
-	die("VIRHE NRO 2: " . $e->getMessage()); 
+
+$conn = new mysqli('localhost','20n_1900','Xm[9ZohbE@OPQovp','20n_1900');
+if($conn->connect_error){
+	die('Yhdistyminen ei onnistunut : '.$conn->connect_error);
+}else{
+	$stmt = $conn->prepare("INSERT INTO 20n_1900.ilmoittumiset(etunimi, sukunimi, sahkoposti, koulutalo, valinta, viesti)
+	VALUES(?, ?, ?, ?, ?, ?)");
+	$stmt->bind_param("ssssss",$etunimi, $sukunimi, $sahkoposti, $koulutalo, $valinta, $viesti);
+	$stmt->execute();
+	echo "Ilmoittuminen onnistui...";
+	$stmt->close();
+	$conn->close();
 }
 
 ?>
